4cb212d2a4a4b2480705012ba097027b13b096a1
FH <homidovfiruz23@gmail.com>
2026-03-02 16:07:59 +0500

    prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,17 @@ module.exports = {
     {
       name: 'manora-front',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p ' + (process.env.PORT || 3001),
+      args: 'start -p ' + (process.env.PORT || 3002),
       exec_mode: 'fork',
       instances: 1,
       watch: false,
       env: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3002,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3002,
       },
     },
   ],
